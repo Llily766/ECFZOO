126216c6a828b7ec4a3fe1b20e8e606d73e5bc6f
BINET aurélie <au.binet21@gmail.com>
2024-04-29 20:14:08 +0200

    images

--- pages/habitats.php
+++ pages/habitats.php
@@ -2,7 +2,7 @@
 
 
 <h1>Habitats</h1>
-<img id="myImage" src="public/assets/Habitats/la_jungle.png" alt="la_jungle" width="300" height="300">
+<img id="myImage" src="assets/Habitats/la_jungle.png" alt="la_jungle" width="300" height="300">
 
 <div id="text" style="display: block;">
     <h4>la_jungle</h4>
@@ -14,12 +14,12 @@
         Les reptiles : crocodiles, alligators, caméléons, serpents, …
         Les marsupiaux : kangourous, koalas, …
         Les pachydermes : éléphants, hippopotames, rhinocéros, …</p>
-    <img src="public/assets/Habitats/tigre.png>"
+    <img src="assets/Habitats/tigre.png">
     <p>Un animal est caractérsé par son prénom:simba (le tigre)
         Prenons l exemple du tigre
         race:Tigre du Bengale</p>
 
-    <img id="myImage" src="public/assets/Habitats/images.png" alt="images" width="300" height="300">
+    <img id="myImage" src="assets/Habitats/images.png" alt="images" width="300" height="300">
 
     <div id="text" style="display: block;">
         <h4>Le marais</h4>
@@ -32,12 +32,12 @@
             Oiseaux. Héron garde-boeufs. Gorgebleue à miroir. ...
             Poissons. Gardon. Grande alose et Alose feinte. ...
             Reptiles. Couleuvre à collier.
-            <img src="public/assets/Habitats/ragondin.png>"
+            <img src="assets/Habitats/ragondin.png">
         <p>un animal est caractérisé par
             son prénom:Ragondin
             sa race:Myocastor coypus, plus simplement, le ragondin, aussi appelé « castor des marais », ou « loutre d'Amérique »,
             appartient à la famille des Myocastoridés, des mammifères rongeurs </p>
-        <img id="myImage" src="public/assets/Habitats/Savane.png" alt="images" width="300" height="300">
+        <img id="myImage" src="assets/Habitats/Savane.png" alt="images" width="300" height="300">
 
         <div id="text" style="display:block">
             <h4>la savane</h4>
@@ -56,7 +56,7 @@
                 son prénom : Giraffon
                 sa race:Girafe du niger (mais beaucoup d'espèces existent)
                 son habitat : la Savane </p>
-            <img src="public/assets/Habitats/girafe.png>"
+            
         </div>
     </div>
 
